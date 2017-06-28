7124a59de4058cfa8fbe1450e60a1afebfd8ac7d
Albert Huang <alberthdev@users.noreply.github.com>
2017-06-28 00:24:28 -0400

    Arduino-ize (and ESP8266-ize) the bcrypt library

--- bcrypt.h
+++ bcrypt.h
@@ -20,44 +20,49 @@
 extern "C" {
 #endif
 
-/*
- * This function expects a work factor between 4 and 31 and a char array to
- * store the resulting generated salt. The char array should typically have
- * BCRYPT_HASHSIZE bytes at least. If the provided work factor is not in the
- * previous range, it will default to 12.
- *
- * The return value is zero if the salt could be correctly generated and
- * nonzero otherwise.
- *
- */
-int bcrypt_gensalt(int workfactor, char salt[BCRYPT_HASHSIZE]);
+#include "Arduino.h"
 
-/*
- * This function expects a password to be hashed, a salt to hash the password
- * with and a char array to leave the result. Both the salt and the hash
- * parameters should have room for BCRYPT_HASHSIZE characters at least.
- *
- * It can also be used to verify a hashed password. In that case, provide the
- * expected hash in the salt parameter and verify the output hash is the same
- * as the input hash. However, to avoid timing attacks, it's better to use
- * bcrypt_checkpw when verifying a password.
- *
- * The return value is zero if the password could be hashed and nonzero
- * otherwise.
- */
-int bcrypt_hashpw(const char *passwd, const char salt[BCRYPT_HASHSIZE],
-		  char hash[BCRYPT_HASHSIZE]);
-
-/*
- * This function expects a password and a hash to verify the password against.
- * The internal implementation is tuned to avoid timing attacks.
- *
- * The return value will be -1 in case of errors, zero if the provided password
- * matches the given hash and greater than zero if no errors are found and the
- * passwords don't match.
- *
- */
-int bcrypt_checkpw(const char *passwd, const char hash[BCRYPT_HASHSIZE]);
+namespace Bcrypt
+{
+    /*
+     * This function expects a work factor between 4 and 31 and a char array to
+     * store the resulting generated salt. The char array should typically have
+     * BCRYPT_HASHSIZE bytes at least. If the provided work factor is not in the
+     * previous range, it will default to 12.
+     *
+     * The return value is zero if the salt could be correctly generated and
+     * nonzero otherwise.
+     *
+     */
+    int gensalt(int workfactor, char salt[BCRYPT_HASHSIZE]);
+    
+    /*
+     * This function expects a password to be hashed, a salt to hash the password
+     * with and a char array to leave the result. Both the salt and the hash
+     * parameters should have room for BCRYPT_HASHSIZE characters at least.
+     *
+     * It can also be used to verify a hashed password. In that case, provide the
+     * expected hash in the salt parameter and verify the output hash is the same
+     * as the input hash. However, to avoid timing attacks, it's better to use
+     * Bcrypt.checkpw when verifying a password.
+     *
+     * The return value is zero if the password could be hashed and nonzero
+     * otherwise.
+     */
+    int hashpw(const char *passwd, const char salt[BCRYPT_HASHSIZE],
+        char hash[BCRYPT_HASHSIZE]);
+    
+    /*
+     * This function expects a password and a hash to verify the password against.
+     * The internal implementation is tuned to avoid timing attacks.
+     *
+     * The return value will be -1 in case of errors, zero if the provided password
+     * matches the given hash and greater than zero if no errors are found and the
+     * passwords don't match.
+     *
+     */
+    int checkpw(const char *passwd, const char hash[BCRYPT_HASHSIZE]);
+};
 
 /*
  * Brief Example
@@ -69,9 +74,9 @@ int bcrypt_checkpw(const char *passwd, const char hash[BCRYPT_HASHSIZE]);
  *	char hash[BCRYPT_HASHSIZE];
  *	int ret;
  *
- *	ret = bcrypt_gensalt(12, salt);
+ *	ret = Bcrypt.gensalt(12, salt);
  *	assert(ret == 0);
- *	ret = bcrypt_hashpw("thepassword", salt, hash);
+ *	ret = Bcrypt.hashpw("thepassword", salt, hash);
  *	assert(ret == 0);
  *
  *
@@ -79,7 +84,7 @@ int bcrypt_checkpw(const char *passwd, const char hash[BCRYPT_HASHSIZE]);
  *
  *	int ret;
  *
- *      ret = bcrypt_checkpw("thepassword", "expectedhash");
+ *      ret = Bcrypt.checkpw("thepassword", "expectedhash");
  *      assert(ret != -1);
  *
  *	if (ret == 0) {
